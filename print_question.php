85a3ddb67cd855803794daa0528f25aa2fddd0c8
Badgun200 <akantogaming@gmail.com>
2019-10-16 10:17:53 +0200

    Print question - můžeš stylovat

--- print_question.php
+++ print_question.php
@@ -1,9 +1,8 @@
 <?php
-  require_once('config/db.php')
-
-  $curr_q = $_POST['curr_q']
-  $sum = $_POST['sum']
-
+  require_once('config/db.php');
+  $curr_q = $_POST['curr_q'];
+  $sum = $_POST['sum'];
+  echo("<span id=\"curr_q\">".$curr_q."</span><span id=\"sum\">".$sum."</span>");
   if()
   $questions = mysqli_query("SELECT question FROM questions WHERE ID=".$curr_q);
   $row = mysqli_fetch_row($row)
@@ -15,4 +14,14 @@
         echo "<div class=\"answer\">".$answers[$i]."</div>";
     }
 ?>
-<button class="submit">
+<input class="submit" type="button">Další</input>
+<script type="text/javascript">
+    document.getElementById("button").onclick = function () {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+      var id = document.getElementById('curr_q').value;
+      var sum = document.getElementById('sum').value;
+      xhr.send("curr_q="+id+"&sum="+sum);
+    };
+</script>
