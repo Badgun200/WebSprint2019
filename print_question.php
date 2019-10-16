873d09f377b0800a0c2671634adc632048a7bb4f
Badgun200 <akantogaming@gmail.com>
2019-10-16 11:05:14 +0200

    funkce getSums

--- print_question.php
+++ print_question.php
@@ -13,6 +13,28 @@
   for($i = 1; $i <= 8; $i+=2) {
         echo "<div class=\"answer\">".$answers[$i]."</div>";
     }
+
+
+//funkci hodíš odpověď jak char A, B, C, D, pak číslo otázky a současné součty jako slovník; vyplyvne novej slovník
+  function getSums($answ, $quest_id, $sums) {
+    $vals = mysqli_query($con, "SELECT vA, vB, vC, vD FROM answers WHERE ID=".$quest_id);
+    $row = mysqli_fetch_row($vals);
+    switch($answ) {
+      case "A":
+        $sums[$row[0]] += 1;
+        break;
+      case "B":
+        $sums[$row[1]] += 1;
+        break;
+      case "B":
+        $sums[$row[2]] += 1;
+        break;
+      case "B":
+        $sums[$row[3]] += 1;
+        break;
+    }
+    return $sums;
+  }
 ?>
 <input class="submit" type="button">Další</input>
 <script type="text/javascript">
